6b3ba055712c5733e8e8dc01daaa0a95b21fea57
redassser <40395425+redassser@users.noreply.github.com>
2019-01-24 17:49:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Enmap = require('enmap');
 const EnmapMongo = require("enmap-mongo");
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "!";
+const prefix = ".";
 client.friend = new Enmap({ provider: new EnmapMongo({
   name: `smashfriend`,
   dbName: `smashbot`,
@@ -19,6 +19,15 @@ client.on("message", (message) => {
   const command = args.shift();  
   if (!message.content.startsWith(prefix) || message.author.bot) return;
   //code starts here lol
+  
+  if (command === "fclist") {
+    const array = client.freind.keyArray()
+    if (array.length === 0) {
+      for (var i = 0; i < array.length; i++) {message.channel.send("``No commands have been made. Use !set to add some!``");return}
+      array[i] = array[i].split(message.guild.id).join("");
+    }
+    message.channel.send("Friend codes are:\n``"+array.join('\n')+"``");
+  }
   
 });
 client.login(process.env.token);
