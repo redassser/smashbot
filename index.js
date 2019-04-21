236b23fdfb5ccdbf13591c9cb38acd2469160323
redassser <40395425+redassser@users.noreply.github.com>
2019-04-21 12:48:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ client.on("message", (message) => {
     for (var i = 0; i < peeps.length; i++) {
       console.log(peeps[i]);
       peeps[i] = client.fetchUser(peeps[i]);
-      console.log(peeps[i]);
-      peeps[i]="Added by "+peeps[i].username+" -> "+client.friend.get(peeps[i]);
+      console.log(peeps[i].username);
+      peeps[i]="Added by "+peeps[i]+" -> "+client.friend.get(peeps[i]);
       console.log(peeps[i]);
     }
     message.channel.send("Friend codes are:\n``"+peeps.join('\n')+"``");
