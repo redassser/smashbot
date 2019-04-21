c35c8de169cc5f41d90089e1620d0d11fe0d98aa
redassser <40395425+redassser@users.noreply.github.com>
2019-04-21 12:40:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,11 @@ client.on("message", (message) => {
     const peeps = client.friend.keyArray()
     if (peeps.length === 0) {message.channel.send("``No friendcodes? ***aaaaaaaaa***``");return}
     for (var i = 0; i < peeps.length; i++) {
-      const name = client.fetchUser(peeps[i]).username;
-      peeps[i]="Added by "+name+" -> "+client.friend.get(peeps[i]);
+      console.log(peeps[i]);
+      peeps[i] = client.fetchUser(peeps[i]).username;
+      console.log(peeps[i]);
+      peeps[i]="Added by "+peeps[i]+" -> "+client.friend.get(peeps[i]);
+      console.log(peeps[i]);
     }
     message.channel.send("Friend codes are:\n``"+peeps.join('\n')+"``");
   }
