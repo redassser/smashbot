ced0efc0c6faebae883a31f3839c799ca2d0ee72
redassser <40395425+redassser@users.noreply.github.com>
2019-05-02 22:21:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,11 +30,9 @@ client.on("message", (message) => {
   }
   if (command === "fclist") {
     const peeps = client.friend.keyArray();
-    if (((array[0]*5)-5)>(peeps.length+5)) {message.channel.send("There aren't that many pages");return;}
+    if (((array[0]*5))>(peeps.length+5)) {message.channel.send("There aren't that many pages");return;}
     if (!isNaN(array[0])) {
       var x = (array[0]*5)-5;
-      console.log(peeps.length);
-      console.log(x)
       if ((peeps.length)>(x+5)) {
         var y = x+5;
       } else {
