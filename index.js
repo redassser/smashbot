9097cbe1173223bf036fa90bb58a816ebcdee936
redassser <40395425+redassser@users.noreply.github.com>
2019-07-01 21:49:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ client.on("message", (message) => {
   if (command === "idadd") {
     if (array.length < 3) {message.channel.send("``.idadd [type] [id] [Title]``"); return;}
     var type = array.shift(); var id = array.shift(); var desc = array.join(" ");
-    if (type != "stage" || "mii" || "replay" || "video") {message.channel.send("``Please use stage, mii, replay or video``");return;}
+    if (type != "stage" || type != "mii" || type != "replay" || type != "video") {message.channel.send("``Please use stage, mii, replay or video``");return;}
     client.online.set(id,[message.author.id,type,desc])
   }
 });
