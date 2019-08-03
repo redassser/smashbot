06d9cf55a6d2e8e395f68ba6a52cbde07638e804
redassser <40395425+redassser@users.noreply.github.com>
2019-08-03 17:30:34 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -2,7 +2,7 @@ module.exports = (client, message) => {
   if (message.author.bot) return;
   if (message.content.indexOf(".") !== 0) return;
 
-  const args = message.content.slice(1).trim().split(/ +/g);
+  const array = message.content.slice(1).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
   const cmd = client.commands.get(command);
