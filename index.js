c7aed158309abfacf54de8accc9ea77f73ce2317
redassser <40395425+redassser@users.noreply.github.com>
2019-08-03 18:17:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -104,39 +104,5 @@ client.on("message", (message) => {
     if (type != "stage" && type != "mii" && type != "replay" && type != "video") {message.channel.send("``Please use stage, mii, replay or video``");return;}
     client.online.set(id,[message.author.id,type,desc])
   }
-  if (command === "setup") {
-    if (!client.mains.has(message.author.id)) {client.mains.set(message.author.id,{"mains":[],"seconds":[],"pockets":[]})}
-    if (array.length < 2) {message.channel.send("``.setup [mains|seconds|pockets] [character number (find in .fighters) to add, or remove if the number is already present]``");return;}
-    const v = array.shift();
-    switch (v) {
-      case "mains":
-        for (var i = 0;i<array.length;i++) {
-          if (!(array[i] in fighter)) {message.channel.send("``Please use a number corresponding to a fighter!``");return;}
-          if ((client.mains.get(message.author.id)["mains"]).includes(array[i])) {client.mains.remove(message.author.id,array[i],"mains");}
-          else {client.mains.push(message.author.id,array[i],"mains")}
-        }
-        message.channel.send("Your mains are "+client.mains.get(message.author.id)["mains"].join(" | "))
-        break;
-      case "seconds":
-        for (var i = 0;i<array.length;i++) {
-          if (!(array[i] in fighter)) {message.channel.send("``Please use a number corresponding to a fighter!``");return;}
-          if ((client.mains.get(message.author.id)["seconds"]).includes(array[i])) {client.mains.remove(message.author.id,array[i],"seconds");}
-          else {client.mains.push(message.author.id,array[i],"seconds")}
-        }
-        message.channel.send("Your secondaries are "+client.mains.get(message.author.id)["seconds"].join(" | "))
-        break;
-      case "pockets":
-        for (var i = 0;i<array.length;i++) {
-          if (!(array[i] in fighter)) {message.channel.send("``Please use a number corresponding to a fighter!``");return;}
-          if ((client.mains.get(message.author.id)["pockets"]).includes(array[i])) {client.mains.remove(message.author.id,array[i],"pockets");}
-          else {client.mains.push(message.author.id,array[i],"pockets")}
-        }
-        message.channel.send("Your pockets are "+client.mains.get(message.author.id)["pockets"].join(" | "))
-        break;
-      default:
-        message.channel.send("``.setup [mains|seconds|pockets] [character number]``")
-        break;
-    }
-  }
 });
 client.login(process.env.token);
