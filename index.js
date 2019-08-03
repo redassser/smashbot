feee653f0eec17b6e2648204b413ef69363d6fea
redassser <40395425+redassser@users.noreply.github.com>
2019-08-03 17:24:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -51,19 +51,6 @@ client.on("message", (message) => {
   const command = array.shift();  
   if (!message.content.startsWith(prefix) || message.author.bot) return;
   //code starts here lol
-  if (command === "smash") {
-    let smashEmbed = new Discord.RichEmbed() 
-    .setColor("#3f9cc6")
-    .setTitle("This is SmashBot for the HCST gaming discord")
-    .setDescription("Use this bot to view friendcodes and share your own!")
-    .addField("See a numbered list with every fighter using","``.fighters [page 1-"+Math.ceil(Object.keys(fighter).length/15)+"]``")
-    .addField("Set up your list of Mains, Secondaries, and Pockets with","``.setup [mains|seconds|pockets] [fighter number]``")
-    .addField("Take a look at your (or someone else's!) Mains, Secondaries, and Pockets with","``.mains [optional ping]``")
-    .addField("Add your friendcode to the list with:","``.fcadd [name] [friendcode]``")
-    .addField("See everyone else's friendcode with:","``.fclist [page 1-"+Math.ceil(client.friend.keyArray().length/5)+"]``")
-    .addField("Share your stage, mii, replay or video with:","``.idadd [stage/mii/replay/video] [id] [Title]``")
-    message.channel.send(smashEmbed);
-  }
   if (command === "fighters") {
     const fighters = Object.keys(fighter)
     if (isNaN(array[0])) {message.channel.send("``.fighters [page number]``");return;}
