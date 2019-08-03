adf9ede144bc07da60fa48d37afad348dbc949ed
redassser <40395425+redassser@users.noreply.github.com>
2019-08-03 18:34:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -71,14 +71,6 @@ client.on("message", (message) => {
     }
     message.channel.send(listEmbed);
   }
-  if (command === "fcadd") {
-    if (array.length < 2) {message.channel.send("``.fcadd [name] [fc]``"); return;}
-    var name = array.shift()
-    var fc = array.join("-")
-    var msg = name+" | "+fc
-    client.friend.set(message.author.id,msg)
-    message.channel.send(msg+" added by "+message.author.username)
-  }
   if (command === "idlist") {
     const ids = client.online.keyArray();
     const pages = Math.ceil(ids/5)
