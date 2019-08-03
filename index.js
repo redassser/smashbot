fd3987a34b625ea4e27debebe471460b08225429
redassser <40395425+redassser@users.noreply.github.com>
2019-08-03 17:19:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,9 +24,26 @@ client.mains = new Enmap({ provider: new EnmapMongo({
   url: process.env.mongo1
 })
 })
-client.on("ready", () => {
-  console.log("Fight!");
-  client.user.setPresence({ game: { name: '.smash' }, status: 'online' });
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
 });
 client.on("message", (message) => {
   const array = message.content.slice(prefix.length).trim().split(/ +/g);
