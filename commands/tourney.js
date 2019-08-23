07c331fda2d6e8b732312148f87b593304eb0500
redassser <40395425+redassser@users.noreply.github.com>
2019-08-23 13:26:01 -0400

    Create tourney.js

---
+++ commands/tourney.js
@@ -0,0 +1,10 @@
+exports.run = (client,message,array) => {
+  const Discord = require("discord.js");
+  if(array[0] ==="create") {
+  
+    let msg1 = new Discord.RichEmbed()
+    
+    client.online
+  
+  }
+}
